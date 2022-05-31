b083c2b88dec2df69655c8c561802849d1715a7e
Gabriel Simplício <gaasimplicio@gmail.com>
2022-05-31 10:49:59 -0300

    versao modificada e atualizada (configurações do professor)

--- routes.js
+++ routes.js
@@ -1,14 +1,26 @@
-const express = require('express')
-const route = express.Router()
+const express = require('express');
+const route = express.Router();
 
-const homeController = require('./src/controllers/homeController')
-const loginController = require('./src/controllers/loginController')
+const homeController = require('./src/controllers/homeController');
+const loginController = require('./src/controllers/loginController');
+const contatoController = require('./src/controllers/contatoController');
 
-//Rotas da home
-route.get('/', homeController.index)
+const { loginRequired } = require('./src/middlewares/middleware');
 
-//Rotas de login
-route.get('/login/register', loginController.index)
-route.post('/login/register', loginController.register)
+// Rotas da home
+route.get('/', homeController.index);
 
-module.exports = route
+// Rotas de login
+route.get('/login/index', loginController.index);
+route.post('/login/register', loginController.register);
+route.post('/login/login', loginController.login);
+route.get('/login/logout', loginController.logout);
+
+// Rotas de contato
+route.get('/contato/index', loginRequired, contatoController.index);
+route.post('/contato/register', loginRequired, contatoController.register);
+route.get('/contato/index/:id', loginRequired, contatoController.editIndex);
+route.post('/contato/edit/:id', loginRequired, contatoController.edit);
+route.get('/contato/delete/:id', loginRequired, contatoController.delete);
+
+module.exports = route;
